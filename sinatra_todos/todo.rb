a8a8291314a6185c2872205734dd7a88b6b12c7d
Marc Battesti <marcandriabattesti@gmail.com>
2017-07-11 19:00:31 +0200

    Sinatra Todos

--- sinatra_todos/todo.rb
+++ sinatra_todos/todo.rb
@@ -123,8 +123,25 @@ post "/lists/:list_id/todos/:todo_id/destroy" do
   redirect "/lists/#{@list_id}"
 end
 
-post "/lists/:list_id/todos/:todo_id/check" do
-  redirect "/lists"
+#Update the status of a todo
+post "/lists/:list_id/todos/:todo_id" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  @todo_id = params[:todo_id].to_i
+  is_completed = params[:completed] == "true"
+  @list[:todos][@todo_id][:completed] = is_completed
+  session[:success] = "The todo has been updated."
+  redirect "/lists/#{@list_id}"
+end
+
+post "/lists/:list_id/complete" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  @list[:todos].each do |todo|
+    todo[:completed] = true
+  end
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@list_id}"
 end
 
 

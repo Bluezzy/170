39908045ef5115b1e290f1fbe17a17ae17855e85
Marc'Andria Battesti <marcandriabattesti@gmail.com>
2017-08-24 15:46:30 +0200

    CMS Project

--- cms_project/cms.rb
+++ cms_project/cms.rb
@@ -1,13 +1,20 @@
 require "sinatra"
 require "sinatra/reloader"
-require "erubis"
+require "tilt/erubis"
 require "redcarpet"
-
-root = File.expand_path("..", __FILE__)
+require "pry"
 
 configure do
   enable :sessions
-  set :session_secret, 'secret'
+  set :session_secret, 'super secret'
+end
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
 end
 
 def render_markdown(text)
@@ -15,31 +22,93 @@ def render_markdown(text)
   markdown.render(text)
 end
 
-def load_cotent(filepath)
-	content = File.read(filepath)
-	case File.extname(filepath)
-	when ".txt"
-		headers["Content-Type"] = "text/plain"
-		File.read(filepath)
-	when ".md"
-		render_markdown(filepath)
-	end
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    erb render_markdown(content)
+  end
 end
 
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  session[:users] = "marc"
+  binding.pry
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
   erb :index
 end
 
-get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+get "/users/signin" do
+	erb :signin
+end
 
-  if File.file?(file_path)
-    load_content(file_path)
+post "/users/signin" do
+  username = params[:username]
+  password = params[:password]
+  session[:message] = "#{params[:username]} is connected"
+  redirect "/"
+end
+
+get "/new" do
+	erb :new
+end
+
+post "/create" do
+  filename = params[:filename].to_s
+
+  if filename.size == 0
+    session[:message] = "A name is required."
+    status 422
+    erb :new
+  else
+    file_path = File.join(data_path, filename)
+
+    File.write(file_path, "")
+    session[:message] = "#{params[:filename]} has been created."
+
+    redirect "/"
+  end
+end
+
+get "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  if File.exist?(file_path)
+    load_file_content(file_path)
   else
     session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
   end
+end
+
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
+  @filename = params[:filename]
+  @content = File.read(file_path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
+end
+
+post "/:filename/delete" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.delete(file_path)
+
+  session[:message] = "#{params[:filename]} has been deleted."
+  redirect "/"
 end
